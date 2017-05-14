48ee05b1f5774f87309323b837864553e2353f62
Celeborn2BeAlive <laurent.noel.c2ba@gmail.com>
2017-05-14 16:57:56 +0200

    Add bat script to clean up downloaded third-party repositories

---
+++ build/clean-third-parties.bat
@@ -0,0 +1,10 @@
+rd /s /q "../third-party/glfw"
+rd /s /q "../third-party/glm"
+rd /s /q "../third-party/imgui"
+rd /s /q "../third-party/assimp"
+rd /s /q "../third-party/json"
+rd /s /q "../third-party/embree"
+rd /s /q "../third-party/openexr"
+rd /s /q "../third-party/stb"
+
+pause
